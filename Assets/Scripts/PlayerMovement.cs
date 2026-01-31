7b0facca84b80e3ed5b04dc96f2dd86ef106ff8d
peter <pgas@itu.dk>
2026-01-31 10:57:24 +0100

    test level and player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpHeight = 2.0f;
+
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    private float horizontalInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
+
+        Debug.Log(isGrounded());
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.AddForce(new Vector2(0, Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y))), ForceMode2D.Impulse);
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f);
+    }
+}
