fd4b0bab50d2688fc07f4e6b3db323d84c44701e
peter <pgas@itu.dk>
2026-01-31 11:39:32 +0100

    wallstick fix, momentum, coyote time

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float speed = 5.0f;
-    [SerializeField] private float jumpHeight = 2.0f;
+    [SerializeField] private float topSpeed = 6.0f;
+    [SerializeField] private float acceleration = 25.0f;
+    [SerializeField] private float jumpHeight = 10.0f;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] [Tooltip("Coyote Time")] private float mayJump = 0.5f;
 
     private Rigidbody2D rb;
     private Collider2D coll;
     private float horizontalInput;
+    private float currentSpeed;
 
     void Start()
     {
@@ -20,21 +23,31 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
 
-        Debug.Log(isGrounded());
+        if (Mathf.Abs(rb.linearVelocity.x) < topSpeed)
+            rb.AddForce(new Vector2(horizontalInput * acceleration, 0));
+
+        currentSpeed = rb.linearVelocity.x;
+        currentSpeed = Mathf.Clamp(currentSpeed, -topSpeed, topSpeed);
+        rb.linearVelocityX = currentSpeed;
+    }
+    bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        //baby's first coyote time
+        if(isGrounded())
+            mayJump = 0.5f;
+        else
+            mayJump -= Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && mayJump > 0f)
         {
             rb.AddForce(new Vector2(0, Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y))), ForceMode2D.Impulse);
+            mayJump = 0f;
         }
-    }
-
-    bool isGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
     }
 }
