8773b201503b513adc82373d9541e7fe48ed026b
peter <pgas@itu.dk>
2026-01-31 18:37:33 +0100

    main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,21 @@
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
- public void PlayGame()
+    private BackGround_switch bgSwitch;
+
+    private void Start()
+    {
+        bgSwitch = GetComponent<BackGround_switch>();
+    }
+
+    public void PlayGame()
     {
         //loads direct to one level, index can be used if multiple levels needed
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene(1);
         Debug.Log("You would load a level");
     }
 
@@ -16,5 +24,19 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("You quit the game");
         //quit won't work in the editor
+    }
+
+    public IEnumerator PlayWithDelay(float delay)
+    {
+        StartCoroutine(bgSwitch.SetColor(bgSwitch.colors[1]));
+        yield return new WaitForSeconds(delay);
+        PlayGame();
+    }
+
+    public IEnumerator QuitWithDelay(float delay)
+    {
+        StartCoroutine(bgSwitch.SetColor(bgSwitch.colors[2]));
+        yield return new WaitForSeconds(delay);
+        QuitGame();
     }
 }
