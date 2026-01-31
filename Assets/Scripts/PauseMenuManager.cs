343eff820ba4796c67b3db3b608e52d0ea8ce385
aleunam97 <118373124+aleunam97@users.noreply.github.com>
2026-01-31 21:34:15 +0100

    Fixed Pausemenu

--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -1,21 +1,34 @@
+using System;
 using UnityEngine;
 
 public class PauseMenuManager : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f; // Resume the game
+        transform.GetChild(0).gameObject.SetActive(false); // Hide pause menu UI
+    }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             if(Time.timeScale == 1f)
             {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
                 Time.timeScale = 0f; // Pause the game
                 transform.GetChild(0).gameObject.SetActive(true); // Show pause menu UI
             }
             else
             {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
                 Time.timeScale = 1f; // Resume the game
                 transform.GetChild(0).gameObject.SetActive(false); // Hide pause menu UI
             }
         }
     }
+
 }
