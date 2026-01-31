429e4f5bd34ea5d0eb60d1894584fe8d854ba76d
peter <pgas@itu.dk>
2026-01-31 20:19:03 +0100

    reset button
    (hold R for 1 second)

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 public class DeathZone : MonoBehaviour
 {
     public GameObject player;
+    private float resetTimer = 0f;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        player.transform.position = new Vector3(1,-1,0);
+        ResetPlayer();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            resetTimer += Time.deltaTime;
+        }
+        else
+            resetTimer = 0f;
+
+        if (resetTimer >= 1f)
+            ResetPlayer();
+
+        Debug.Log(resetTimer);
+    }
+
+    private void ResetPlayer()
+    {
+        if (player != null) 
+            player.transform.position = new Vector3(1, -1, 0);
+        resetTimer = 0f;
     }
 }
